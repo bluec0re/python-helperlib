31e6806f1d45450bcc71ccae21aef1bba7d6befd
BlueC0re <coding@bluec0re.eu>
2014-06-27 09:50:33 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'A collection of useful python modules',
+    'author': 'bluec0re',
+    'url': 'https://github.com/bluec0re/python-helperlib.git',
+    'download_url': 'https://github.com/bluec0re/python-helperlib/archive/master.zip',
+    'author_email': '',
+    'version': '0.1',
+    'install_requires': [],
+    'packages': ['helperlib'],
+    'scripts': [],
+    'name': 'python-helperlib'
+}
+
+setup(**config)
