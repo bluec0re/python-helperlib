28c0ce5099367720f47f6c0127ec41e3d1a616e1
BlueC0re <coding@bluec0re.eu>
2014-07-29 21:30:53 +0200

    Best practices

--- helperlib/__init__.py
+++ helperlib/__init__.py
@@ -1,4 +1,13 @@
 from __future__ import absolute_import, unicode_literals
 
-from .internal import *
+from .internal import (
+        done, fail, prompt, info, success,
+        error, exc, warning, debug
+        )
 from . import spinner
+from . import terminal
+from . import exception
+
+__all__ = ['spinner', 'done', 'fail', 'prompt',
+           'info', 'success', 'error', 'exc',
+           'warning', 'debug', 'terminal', 'exception']
--- helperlib/exception.py
+++ helperlib/exception.py
@@ -4,9 +4,22 @@ import logging
 import sys
 from traceback import format_exception
 
+
+__all__ = [
+     'add_exceptionhook', 'rm_exceptionhook', 'uncaught_hook', 'install_hook'
+]
+
 _hooks = []
 
 def uncaught_hook(exc_class, exc_inst, trb):
+    """
+
+    Keyword arguments:
+    exc_class --
+    exc_inst --
+    trb --
+
+    """
     if isinstance(exc_inst, KeyboardInterrupt):
         sys.exit(0)
     elif isinstance(exc_inst, SystemExit):
@@ -17,20 +30,42 @@ def uncaught_hook(exc_class, exc_inst, trb):
 
 
 def hook(*args, **kwargs):
+    """
+
+    Keyword arguments:
+    *args --
+    **kwargs --
+
+    """
     for h in _hooks:
         h(*args, **kwargs)
 
 
 def add_exceptionhook(h):
+    """
+    add exception hook to the hook listener
+
+    Keyword arguments:
+    h -- the hook
+    """
     _hooks.insert(0, h)
 
 
 def rm_exceptionhook(h):
+    """
+    rm exception hook from the hook listener
+
+    Keyword arguments:
+    h -- the hook
+    """
     i = _hooks.find(h)
     if i > -1:
         del _hooks[i]
 
 
 def install_hook():
+    """
+    install the uncaught exception hook listener
+    """
     _hooks.append(uncaught_hook)
     sys.excepthook = hook
